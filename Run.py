d7d56c3da88d98ff1aca03410f3a6b69340699bc
sumanshusamarora <sumanshusamarora@gmail.com>
2019-08-16 12:57:57 +1000

    Updated save function to untilize full data

--- Run.py
+++ Run.py
@@ -1,6 +1,6 @@
 #Importing libraries and packages including out in-house class
 import sys
-sys.path.append(r'C:\NotBackedUp\Common Repos\Text Classification')
+sys.path.append(r'--Enter path of git repo---')
 from TextClassifier import Classification_model
 import pandas as pd
 import win32com.client
@@ -9,7 +9,7 @@ xlapp = win32com.client.Dispatch("Excel.Application")
  
 #Uploading labelled data to python, please ensure your data file has only two columns 1) Document's text and 2) Label
 #In case there are multiple text columns that need to be used, please concatenate them in excel in one column
-data_file = r"C:\NotBackedUp\Common Repos\Text Classification\Data\Old Training Data.xlsx"
+data_file = r"--Enter data file path---"
 data = pd.read_excel(data_file)
 data.columns = ["text", "label"]
  
@@ -38,4 +38,4 @@ EMC.save_model(AreYouSure=False)
 #*********Prediction can be done using below function and passing it the new unlabeled text data as input
 EMC_P = Classification_model(Texts=orig_df['text'], model_training=False)
 CleanCorpus_P = EMC_P.CleanCorpus()
-y_pred, y_pred_copy, Y_pred_prob= EMC_P.predict_class()
+y_pred, y_pred_copy, Y_pred_prob, Y_pred_classed= EMC_P.predict_class()
